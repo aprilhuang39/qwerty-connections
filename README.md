1f61dc42b96eccdb8e5edbddbca3cccce342823c
Carter Tran <carter.tran@gmail.com>
2025-05-05 16:30:39 -0700

    add dataset and remove wget script

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
 # qwerty-connections
-
-## Setup
-
-To download the dataset, use `./download_dataset.sh`
--- download_dataset.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-# Download the dataset from PhysioNet
-wget -r -N -c -np https://physionet.org/files/nqmitcsxpd/1.0.0/
