be8571c06521a0bd587ec5f8c3cd0baa7aa9ea6a
Carter Tran <carter.tran@gmail.com>
2025-05-05 16:19:39 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # qwerty-connections
+
+## Setup
+
+To download the dataset, use `./download_dataset.sh`
---
+++ download_dataset.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+# Download the dataset from PhysioNet
+wget -r -N -c -np https://physionet.org/files/nqmitcsxpd/1.0.0/
